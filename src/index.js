fa19094b49d726900934ef56e5453adb4f79efac
鎌田遼 <kamataryo@users.noreply.github.com>
2020-12-18 13:41:39 +0900

    Test caching

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 // simply echo qs
-const handler = async (event, context) => {
+const handler = async event => {
     return {
       statusCode: 200,
+      headers: {
+        'Cache-Control': event.path === "/no-cache" ? 'no-cache, max-age=0' : "max-age=120"
+      },
       body: JSON.stringify({
         queryStringParameters: event.queryStringParameters
       })
